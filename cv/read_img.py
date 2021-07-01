2d7b00d52777b853a361e0e609ae71a024cec2a6
Ankit Shaw <25771394+ankitshaw@users.noreply.github.com>
2021-07-01 19:00:09 +0000

    face detection, contour, text, line, rectangle, blur

--- cv/read_img.py
+++ cv/read_img.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-image = cv.imread("images/img.jpg")
+image = cv.imread("images/faces.jpg")
 
 #1
 #bgr_image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
@@ -25,10 +25,46 @@ image = cv.imread("images/img.jpg")
 
 
 #6
-gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-ret, threshold_image = cv.threshold(image, 150, 255, 0)
-cv.imwrite("output/img.jpg",gray_image)
-cv.imwrite("output/img2.jpg",threshold_image)
+# gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+# ret, threshold_image = cv.threshold(image, 150, 255, 0)
+# cv.imwrite("output/img.jpg",gray_image)
+
+#7
+# blurred = cv.GaussianBlur(image, (51, 51), 0)
+
+#8
+#cv.rectangle(image, (600, 200), (1300, 800), (0, 255, 255), 10)
+
+#9
+#cv.line(image, (1000, 100), (1000, 800), (0, 0, 255), 5)
+
+#10
+#cv.putText(image, "Testing putText", (500, 400),cv.FONT_HERSHEY_SIMPLEX, 5, (30, 105, 210), 20) 
+
+
+
+#11
+# face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+# gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+# faces = face_cascade.detectMultiScale(
+#     gray,
+#     scaleFactor= 1.1,
+#     minNeighbors= 5,
+#     minSize=(10, 10)
+# )
+# faces_detected = format(len(faces)) + " faces detected!"
+# print(faces_detected)
+# # Draw a rectangle around the faces
+# for (x, y, w, h) in faces:
+#     cv.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
+#12
+# imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+# ret, thresh = cv.threshold(imgray, 127, 255, 0)
+# contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# cv.drawContours(image, contours, -1, (0,255,0), 3)
+
+cv.imwrite("output/img.jpg",image)
 
 
 def viewImage(image, name_of_window):
